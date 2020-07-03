3228f6d5f00ebebe035c574c62f6272834b11457
rauschnita <rausch.nita@gmail.com>
2020-07-03 15:00:05 +0200

    fixed amount validation

--- js/main.js
+++ js/main.js
@@ -2,7 +2,27 @@ function calcAmount() {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']");
     let showAmount = document.querySelector("span.show-amount");
-    let amount = parseInt(amountInput.value) * price;
+    let amountNumber = parseInt(amountInput.value);
+    amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
 
-    showAmount.innerHTML = amount;
+
+    if (amountNumber > 10) {
+        alert("Maximum 10 db terméket vásárolhat!")
+    } else if (amountNumber < 1) {
+        alert("Minimum 1 db terméket kell vásárolnia!");
+    } else {
+        let amount = parseInt(amountInput.value) * price;
+        showAmount.innerHTML = amount;
+    }
+
+    function ValidateEmail() {
+        if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)) {
+            return (true)
+        }
+        alert("You have entered an invalid email address!")
+        return (false)
+    }
+
+
+
 }
