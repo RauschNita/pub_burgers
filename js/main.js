1da1ad9405bdcaf4caaf7e7790fc8cc63108eed2
rauschnita <rausch.nita@gmail.com>
2020-07-06 15:24:48 +0200

    distributed into more functions

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 function calcAmount() {
     let price = 1000;
     let amountInput = document.querySelector("input[name='amount-input']");
-    let showAmount = document.querySelector("span.show-amount");
     let amountNumber = parseInt(amountInput.value);
     amountNumber = isNaN(amountNumber) ? 0 : amountNumber;
+    showSumPrice(price, amountNumber);
+}
 
+function showSumPrice(price = 1000, amountNumber = 1) {
+    let showAmount = document.querySelector("span.show-amount");
     if (amountNumber > 10) {
         alert("Maximum 10 db terméket vásárolhat!")
     } else if (amountNumber < 1) {
         alert("Minimum 1 db terméket kell vásárolnia!");
     } else {
-        let amount = parseInt(amountInput.value) * price;
+        let amount = amountNumber * price;
         showAmount.innerHTML = amount;
     }
-
 }
-
+ 
 function validateEmail(inputText) {
     const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     let emailInput = document.querySelector("input[name='email-input']");
@@ -25,5 +27,4 @@ function validateEmail(inputText) {
     }
     alert("You have entered an invalid email address!")
     return (false)
-
 }
